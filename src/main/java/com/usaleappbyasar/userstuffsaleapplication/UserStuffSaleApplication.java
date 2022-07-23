0c75ce709c89956a1fcbd0c6e8336c2323da3c8e
BirkanYasar <birkanyasar.366@gmail.com>
2022-07-23 13:50:19 +0300

    Object models are created and edited.

---
+++ src/main/java/com/usaleappbyasar/userstuffsaleapplication/UserStuffSaleApplication.java
@@ -0,0 +1,13 @@
+package com.usaleappbyasar.userstuffsaleapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserStuffSaleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserStuffSaleApplication.class, args);
+    }
+
+}
